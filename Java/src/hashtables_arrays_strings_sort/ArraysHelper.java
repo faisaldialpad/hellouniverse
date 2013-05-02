37d7591bb8da7c4a1e2d87268e15b1fe0f45a7c1
Faisal Rahman (BlueBat) <md.faisal.rahman@gmail.com>
2013-05-02 16:31:26 -0700

    max sequence added

--- Java/src/hashtables_arrays_strings_sort/ArraysHelper.java
+++ Java/src/hashtables_arrays_strings_sort/ArraysHelper.java
@@ -24,4 +24,29 @@ public class ArraysHelper {
 		  }
 		  return ret;
 	  }
+	public static int[] maxSequence(int[] input){
+		int curSeqLength=1;
+		int maxSeqLength=1;
+		int startIndex=0;
+		for(int i=1;i<input.length;i++){
+			if(input[i]==input[i-1]+1){
+				curSeqLength++;
+			}
+			else{
+				if(curSeqLength>maxSeqLength){
+					maxSeqLength=curSeqLength;
+					startIndex=i-curSeqLength;
+				}
+				curSeqLength=1;
+			}
+		}
+		if(curSeqLength>maxSeqLength){
+			maxSeqLength=curSeqLength;
+			startIndex=input.length-curSeqLength;
+		}
+		int []output= new int[maxSeqLength];
+		for(int i=0;i<maxSeqLength;i++)
+			output[i]=input[i+startIndex];
+		return output;
+	}
 }
--- Java/src/unittest/hashtables_arrays_strings_sort/ArraysHelperTest.java
+++ Java/src/unittest/hashtables_arrays_strings_sort/ArraysHelperTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import hashtables_arrays_strings_sort.ArraysHelper;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 
@@ -69,6 +70,12 @@ public class ArraysHelperTest {
 		result.add(r2);
 		result.add(r3);
 		assertTrue(result.equals(ArraysHelper.permuteWithoutDuplicate(list)));
+	}
+	@Test
+	public final void testmaxseq() {
+		int []a= { 74, 63, 7, 8, 9, 10, 11, 12, 78, 2, 21, 22, 23, 25, 79, 78, 76, 77, 71, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(ArraysHelper.maxSequence(a)));
 	}
 
 }
