aae60858db116761b59b660406d4ed408e51a1fd
Faisal Rahman (Windows) <md.faisal.rahman@gmail.com>
2013-05-16 18:43:03 -0700

    dp solution for Max Value Contigious Sub Seq

--- Java/src/hashtables_arrays_strings_sort/ArraysHelper.java
+++ Java/src/hashtables_arrays_strings_sort/ArraysHelper.java
@@ -27,31 +27,7 @@ public class ArraysHelper {
 		  }
 		  return ret;
 	  }
-	public static int[] maxSequence(int[] input){
-		int curSeqLength=1;
-		int maxSeqLength=1;
-		int startIndex=0;
-		for(int i=1;i<input.length;i++){
-			if(input[i]==input[i-1]+1){
-				curSeqLength++;
-			}
-			else{
-				if(curSeqLength>maxSeqLength){
-					maxSeqLength=curSeqLength;
-					startIndex=i-curSeqLength;
-				}
-				curSeqLength=1;
-			}
-		}
-		if(curSeqLength>maxSeqLength){
-			maxSeqLength=curSeqLength;
-			startIndex=input.length-curSeqLength;
-		}
-		int []output= new int[maxSeqLength];
-		for(int i=0;i<maxSeqLength;i++)
-			output[i]=input[i+startIndex];
-		return output;
-	}
+	
 	public static int rotations(String s){
 		if(s=="") return 1;
 		int []hash= new int[256];
