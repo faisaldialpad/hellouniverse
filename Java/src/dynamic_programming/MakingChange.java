53e405d724379a58a48ae45c81bd199a52c10d0d
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-05-20 17:36:05 -0700

    making change added

---
+++ Java/src/dynamic_programming/MakingChange.java
@@ -0,0 +1,16 @@
+package dynamic_programming;
+
+public class MakingChange extends UnboundedKnapsack {
+	public MakingChange(int money, int[] changes) throws Exception {
+		super(money, changes, generateValuesforKnapSack(changes));
+	}
+	public static int[] generateValuesforKnapSack(int[] weights){
+		if(weights==null || weights.length==0)
+			return new int[0];
+		int []ret= new int[weights.length];
+		for(int i=0;i<weights.length;i++)
+			ret[i]=-1;
+		return ret;
+	}
+
+}
