73c329fa8815e5ccbbe17ac4fe14cf38c927ae69
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-05-24 15:40:24 -0700

    StringAdd test added

--- Java/src/hashtables_arrays_strings_sort/StringAdd.java
+++ Java/src/hashtables_arrays_strings_sort/StringAdd.java
@@ -1,21 +1,29 @@
 package hashtables_arrays_strings_sort;
 
 public class StringAdd {
-	public static String add(String s1, String s2, int carry){
-		if(s1.length()==0 || s2.length()==0){
-			if(carry==0)
-				return s1.length()==0?s2:s1;
-			else
-				return ""+carry;
+	private String s1;
+	private String s2;
+	public StringAdd(String s1, String s2){
+		this.s1=s1;
+		this.s2=s2;
+	}
+	public String add() throws Exception{
+		return addRecursive(s1, s2, 0);
+	}
+	private String addRecursive(String s1, String s2, int carry) throws Exception{
+		if(NullOrEmpty(s1) && NullOrEmpty(s2) && carry==0){
+			return "";
 		}
-		int num1=s1.charAt(s1.length()-1) - '0';
-		int num2=s2.charAt(s2.length()-1) - '0';
+		int num1=NullOrEmpty(s1)?0:s1.charAt(s1.length()-1) - '0';
+		int num2=NullOrEmpty(s2)?0:s2.charAt(s2.length()-1) - '0';
+		if(num1<0 ||num1>9 ||num2<0||num2>9)
+			throw new Exception("Wrong Input.");
 		int sum=num1+num2+carry;
 		carry=sum/10;
 		sum=sum%10;
-		return add(s1.substring(0,s1.length()-1),s2.substring(0,s2.length()-1),carry)+sum;
+		return addRecursive(NullOrEmpty(s1)?"":s1.substring(0,s1.length()-1),NullOrEmpty(s2)?"":s2.substring(0,s2.length()-1),carry)+sum;
 	}
-	public static void main(String[] args) {
-		System.out.println(add("9","9",0));
+	private boolean NullOrEmpty(String s){
+		return s==null||s.length()==0;
 	}
 }
--- Java/src/unittests/StringAddTest.java
+++ Java/src/unittests/StringAddTest.java
@@ -1,24 +1,41 @@
 package unittests;
 
 import static org.junit.Assert.*;
+import hashtables_arrays_strings_sort.StringAdd;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class StringAddTest {
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+	StringAdd sa;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		assertNotNull(new StringAdd(null, null));
 	}
 
 	@Test
-	public void testAdd() {
-		fail("Not yet implemented");
+	public void testAddBoundary1() throws Exception {
+		sa = new StringAdd(null, null);
+		assertEquals("",sa.add());
+	}
+	@Test
+	public void testAddBoundary2() throws Exception {
+		sa = new StringAdd("", "");
+		assertEquals("",sa.add());
+	}
+	@Test
+	public void testAddBoundary3() {
+		sa = new StringAdd("988u90", "1");
+		try {
+			assertEquals("",sa.add());
+		} catch (Exception e) {
+			assertEquals("Wrong Input.", e.getMessage());
+		}
+	}
+	@Test
+	public void testAddNormal1() throws Exception {
+		sa = new StringAdd("897982347612", "8768732648");
+		assertEquals("906751080260",sa.add());
 	}
 
 }
