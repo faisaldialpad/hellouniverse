aa9a3ec3f9d8e3cb958e61b2ff7f1ccb79a6ce72
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-05-23 23:23:14 -0700

    Last n elements added

---
+++ Java/src/linkedlists/Last_N_Elements.java
@@ -0,0 +1,27 @@
+package linkedlists;
+
+
+public class Last_N_Elements {
+	LNode<Integer> list;
+	public Last_N_Elements(LNode<Integer> list) {
+		this.list=list;
+	}
+	public LNode<Integer> calculate(int n){
+		LNode<Integer> t1=list;
+		LNode<Integer> t2=list;
+		while(t1!=null && n>0){
+			t1=t1.next;
+			n--;
+		}
+		if(n>0)
+			return null;
+		else{
+			while(t1!=null){
+				t1=t1.next;
+				t2=t2.next;
+			}
+		}
+		return t2;
+	}
+
+}
