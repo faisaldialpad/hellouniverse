eaf0c8011ce795b9fca7db4ff0daef5042c890d7
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-05-24 17:03:52 -0700

    Running Duplicate added

---
+++ Java/src/hashtables_arrays_strings_sort/RunningDuplicate.java
@@ -0,0 +1,18 @@
+package hashtables_arrays_strings_sort;
+
+public class RunningDuplicate {
+	private String s;
+	public RunningDuplicate(String s){
+		this.s=s;
+	}
+	public String calculate(){
+		return calculateRecursive(s,"");
+	}
+	private static String calculateRecursive(String s, String prefix){
+		if(s.equals("")) return "";
+		char c=s.charAt(0);
+		prefix+=""+c+c;
+		String next=calculateRecursive(s.substring(1),prefix);
+		return next.equals("")?prefix:prefix+" "+next;
+	}
+}
