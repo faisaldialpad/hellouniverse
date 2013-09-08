00ddd8623644f1a91fd9920e3ba1bf4b5b893ff5
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-09-07 19:32:43 -0700

    StringSimilarity got accepted in Hackerrank Website

--- Java/src/strings/StringSimilarity.java
+++ Java/src/strings/StringSimilarity.java
@@ -1,29 +1,58 @@
 package strings;
 
-public class StringSimilarity {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
-	public StringSimilarity() {
-		// TODO Auto-generated constructor stub
-	}
+import java.util.Scanner;
+
+public class StringSimilarity {
+	/**
+	 * This one runs faster
+	 */
 	public static int calculate(String s){
-		int count=s.length();
-		for(int i=1;i<s.length();i++){
-			count+=similarCount(s,s.substring(i));
+		char[]arr=s.toCharArray();
+		int length=arr.length;
+		int count=length;
+		for(int i=1;i<length;i++){
+			int len=length-i;
+			int j=0;
+			for(;j<len;j++)
+				if(arr[j]!=arr[j+i]){
+					break;
+				}
+			count+=j;
 		}
 		return count;
 	}
-	private static int similarCount(String s, String sub){
-		for(int i=0;i<sub.length();i++)
-			if(s.charAt(i)!=sub.charAt(i)) 
-				return i;
-		return sub.length();
+	/**
+	 * This one runs slower, but easier to understand
+	 */
+	public static int calculateOriginal(String s){
+		int length=s.length();
+		int count=length;
+		for(int i=1;i<length;i++){
+			count+=similarCountOpt(s,i,length);
+		}
+		return count;
 	}
-   /* public static void main(String[] args) {
+	private static int similarCountOpt(String s, int sub_i, int length){
+		int len=length-sub_i;
+		for(int i=0;i<len;i++)
+			if(s.charAt(i)!=s.charAt(i+sub_i)) 
+				return i;
+		return len;
+	}
+
+   public static void main(String[] args) throws FileNotFoundException {
+	    final long startTime = System.currentTimeMillis();
+	   	System.setIn(new FileInputStream("/Users/rfaisal/github/hellouniverse/Java/src/strings/input.txt"));
 		Scanner scanner = new Scanner( System.in );
 		int n=scanner.nextInt();
 		for(int i=0;i<n;i++){
 			String s=scanner.next();
 			System.out.println(calculate(s));
 		}
-	}*/
+		final long endTime = System.currentTimeMillis();
+		System.out.println("Total execution time: " + (endTime - startTime) );
+	}
 }
--- Java/src/unittests/StringSimilarityTest.java
+++ Java/src/unittests/StringSimilarityTest.java
@@ -15,8 +15,16 @@ public class StringSimilarityTest {
 
 	@Test
 	public void testCalculate() {
+		final long startTime = System.currentTimeMillis();
 		assertEquals(11, StringSimilarity.calculate("ababaa"));
+		assertEquals(11, StringSimilarity.calculate("abcabbca"));
+		assertEquals(17556, StringSimilarity.calculate("abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababab"));
 		assertEquals(3, StringSimilarity.calculate("aa"));
+		assertEquals(3828, StringSimilarity.calculate("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+		assertEquals(32640, StringSimilarity.calculate("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+		assertEquals(92665, StringSimilarity.calculate("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"));
+		final long endTime = System.currentTimeMillis();
+		System.out.println("Total execution time: " + (endTime - startTime) );
 	}
 
 }
