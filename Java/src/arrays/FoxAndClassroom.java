1e81a40c7c180264f473de38a12113e05f0c05b9
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-10-23 10:47:05 -0400

    FoxAndClassroom solution improved

--- Java/src/arrays/FoxAndClassroom.java
+++ Java/src/arrays/FoxAndClassroom.java
@@ -5,6 +5,26 @@ import java.util.HashSet;
 
 public class FoxAndClassroom {
 	public static String ableTo(int m, int n){
+		HashSet<Point> overallVisited=new HashSet<Point>();
+		for(int i=0;i<m;i++){
+			for(int j=0;j<n;j++){
+				Point cur=new Point(i,j);
+				if(!overallVisited.contains(cur)){
+					HashSet<Point> visited=new HashSet<Point>();
+					while(!visited.contains(cur) && visited.size() < m*n){
+						visited.add(cur);
+						cur=next(cur,m,n);
+					}
+					if(visited.size() == m*n){
+						return "Possible";
+					}
+					overallVisited.addAll(visited);
+				}
+			}
+		}
+		return "Impossible";
+	}
+	public static String ableTo_slow(int m, int n){
 		for(int i=0;i<m;i++){
 			for(int j=0;j<n;j++){
 				Point cur=new Point(i,j);
