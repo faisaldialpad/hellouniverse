e782eccced570034ed7e84a4e2d7a6359ff72fef
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-11-11 11:48:11 -0500

    Logic for trimming original tree is also added

--- Java/src/trees_graphs/BTLeavesToDLL.java
+++ Java/src/trees_graphs/BTLeavesToDLL.java
@@ -28,6 +28,36 @@ public class BTLeavesToDLL {
 			}
 		}
 	}
+	public static TNode<Integer> getLeavesAndTrimOriginal(TNode<Integer> root){
+		return getLeavesAndTrimOriginalRec(root,null,false);
+	}
+	private static TNode<Integer> getLeavesAndTrimOriginalRec(TNode<Integer> root,TNode<Integer> parent,boolean isLeftChild){
+		if(root==null){
+			return null;
+		}
+		if(isLeaf(root)){
+			if(parent!=null){
+				if(isLeftChild) parent.left=null;
+				else parent.right=null;
+			}
+			return root;
+		}
+		else{
+			TNode<Integer> left=getLeavesAndTrimOriginalRec(root.left,root,true);
+			if(left!=null){
+				TNode<Integer> temp=left;
+				while(temp.right!=null) temp=temp.right;
+				temp.right=getLeavesAndTrimOriginalRec(root.right,root,false);
+				if(temp.right != null){//boundary
+					temp.right.left=temp;
+				}
+				return left;
+			}
+			else{//boundary
+				return getLeavesAndTrimOriginalRec(root.right,root,false);
+			}
+		}
+	}
 	private static boolean isLeaf(TNode<Integer> root){
 		if(root==null) return false; //if it is not a node it is not a leaf
 		else return root.left == null && root.right==null;
--- Java/src/unittests/BTLeavesToDLLTest.java
+++ Java/src/unittests/BTLeavesToDLLTest.java
@@ -28,5 +28,31 @@ public class BTLeavesToDLLTest {
 		assertEquals(new Integer(10), leaves.right.right.right.left.left.data);
 		assertEquals(new Integer(4), leaves.right.right.right.left.left.left.data);
 	}
+	@Test
+	public void testGetLeavesAndTrimOriginal() {
+		TNode<Integer> r= new TNode<Integer>(20);
+		r.left= new TNode<Integer>(8);
+		r.left.left= new TNode<Integer>(4);
+		r.left.right= new TNode<Integer>(12);
+		r.left.right.left= new TNode<Integer>(10);
+		r.left.right.right= new TNode<Integer>(14);
+		r.right= new TNode<Integer>(22);
+		r.right.right= new TNode<Integer>(25);
+		TNode<Integer> leaves=BTLeavesToDLL.getLeavesAndTrimOriginal(r);
+		assertEquals(new Integer(4), leaves.data);
+		assertEquals(new Integer(10), leaves.right.data);
+		assertEquals(new Integer(14), leaves.right.right.data);
+		assertEquals(new Integer(25), leaves.right.right.right.data);
+		assertEquals(new Integer(14), leaves.right.right.right.left.data);
+		assertEquals(new Integer(10), leaves.right.right.right.left.left.data);
+		assertEquals(new Integer(4), leaves.right.right.right.left.left.left.data);
+		assertEquals(new Integer(8), r.left.data);
+		assertEquals(new Integer(22), r.right.data);
+		assertEquals(new Integer(12), r.left.right.data);
+		assertNull(r.left.left);
+		assertNull(r.left.right.left);
+		assertNull(r.left.right.right);
+		assertNull(r.right.right);
+	}
 
 }
