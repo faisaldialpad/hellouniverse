6cd22642e71df1e9849a73e568b4f3f732a2b112
Faisal Rahman <md.faisal.rahman@gmail.com>
2013-12-30 14:33:58 -0500

    new version of StringSimilarity added

--- Java/src/strings/StringSimilarity.java
+++ Java/src/strings/StringSimilarity.java
@@ -5,10 +5,50 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 */
 public class StringSimilarity {
+	
+	/**
+	 * This one runs even faster
+	 */
+	public static int calculate(String s){
+		char[]arr=s.toCharArray();
+		int length=arr.length;
+		int count=length;
+		int[]T=getTable(arr);
+		for(int i=1;i<length;i++){
+			int len=length-i;
+			int j=0;
+			for(;j<len;j++)
+				if(arr[j]!=arr[j+i]){
+					if(i+1<length) i=T[i+1]-1;
+					break;
+				}
+			count+=j;
+		}
+		return count;
+	}
+	private static int[]getTable(char[]arr){
+		int []T= new int[arr.length];
+		if(arr[arr.length-1]==arr[0]){
+			T[arr.length-1]=arr.length-1;
+		}
+		else{
+			T[arr.length-1]=arr.length;
+		}
+		for(int i=arr.length-2;i>=0;i--){
+			if(arr[i]==arr[0]){
+				T[i]=i;
+			}
+			else{
+				T[i]=T[i+1];
+			}
+		}
+		return T;
+	}
+	
 	/**
 	 * This one runs faster
 	 */
-	public static int calculate(String s){
+	/*public static int calculate(String s){
 		char[]arr=s.toCharArray();
 		int length=arr.length;
 		int count=length;
@@ -22,7 +62,7 @@ public class StringSimilarity {
 			count+=j;
 		}
 		return count;
-	}
+	}*/
 	/**
 	 * This one runs slower, but easier to understand
 	 */
