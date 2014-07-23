2384ca679823fc88da385b32e38ae793c3e15620
Faisal Rahman <md.faisal.rahman@gmail.com>
2014-07-23 15:26:57 -0400

    nth prime is optimized

--- C/math/n_primes.c
+++ C/math/n_primes.c
@@ -3,6 +3,7 @@
 * Problem contributors: Faisal Rahman
 **/
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
 
 /**
@@ -19,19 +20,44 @@ int is_prime(int p){
 }
 
 /**
-* USAGE: ./a.out 5 
+* USAGE: ./a.out 5
+* the nth prime 
 **/
 int main(int argc, char *argv[]){
 	if(argc<2) return 1;
 	int total = atoi(argv[1]);
 	int i=1;
 	int count=0;
+	printf("\nOriginal: ");
 	while(count<total){
 		if(is_prime(i)){
-			printf("%d ",i);
+			//printf("%d ",i);
 			count++;
 		}
 		i++;
 	}
+	printf("%d ",i - 1);
+        // method 2
+	printf("\nOptimized: ");
+        int* prime_arr = malloc(total*sizeof(int));
+        prime_arr[0] = 2;
+        count = 1;
+	i = 3;
+        while(count<total){
+		int flag =0;
+		for(int j=0;j<count;j++){
+			if(i%prime_arr[j] == 0){
+				flag =1; 
+				break;
+			}		
+		}
+		if(flag == 0){ 
+			//printf("%d ",i);
+			prime_arr[count] = i;	
+			count++;
+		}
+		i++;
+	}
+	printf("%d ",prime_arr[count-1]);
 	return 0;
 }
