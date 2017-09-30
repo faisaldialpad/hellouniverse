3e7df067d30e1eb132df5b9c9114c7a1ca23d7cd
Faisal Rahman <md.faisal.rahman@gmail.com>
2017-09-29 23:31:08 -0700

    construct tree from preorder inorder added

---
+++ CSharp/HelloUniverse.Tests/TreeFromPreorderInorderTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace HelloUniverse.Tests
+{
+    [TestFixture]
+    public class TreeFromPreorderInorderTests
+    {
+        [Test]
+        public void CornerCases()
+        {
+            Assert.AreEqual(null, TreeFromPreorderInorder.BuildTree(null, null));
+            Assert.AreEqual(null, TreeFromPreorderInorder.BuildTree(new int[0], new int[0]));
+            Assert.AreEqual(new TreeFromPreorderInorder.TreeNode(5).val, TreeFromPreorderInorder.BuildTree(new int[]{5}, new int[]{5}).val);
+        }
+
+        [Test]
+        public void NormalCases()
+        {
+            var t = TreeFromPreorderInorder.BuildTree(new[] {1, 2, 4, 5, 3, 6}, new[] {4, 2, 5, 1, 6, 3});
+            Assert.AreEqual(1, t.val);
+            Assert.AreEqual(2, t.left.val);
+            Assert.AreEqual(3, t.right.val);
+            Assert.AreEqual(4, t.left.left.val);
+            Assert.AreEqual(5, t.left.right.val);
+            Assert.AreEqual(6, t.right.left.val);
+        }
+    }
+}
---
+++ CSharp/HelloUniverse/TreeFromPreorderInorder.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloUniverse
+{
+    public class TreeFromPreorderInorder
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x)
+            {
+                val = x;
+            }
+        }
+
+        public static TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            if (preorder == null || preorder.Length == 0 || inorder == null || inorder.Length == 0)
+            {
+                return null;
+            }
+
+            if (preorder.Length != inorder.Length)
+            {
+                throw new Exception("Length of both arrays must be same");
+            }
+
+            return BuildTree(preorder, inorder, 0, preorder.Length - 1, 0, inorder.Length - 1);
+        }
+        private static TreeNode BuildTree(int[] preorder, int[] inorder, int sPre, int ePre, int sIn, int eIn)
+        {
+            if (sPre > ePre || sIn > eIn)
+            {
+                return null;
+            }
+
+            var root = new TreeNode(preorder[sPre]);
+            var set = new HashSet<int>();
+            var rootPositionInOrder = 0;
+            for (var i = sIn; i <= eIn; i++)
+            {
+                if (inorder[i] == preorder[sPre])
+                {
+                    rootPositionInOrder = i;
+                    break;
+                }
+
+                set.Add(inorder[i]);
+            }
+
+            var leftPreEnd = sPre + 1;
+            while (leftPreEnd <= ePre && set.Contains(preorder[leftPreEnd]))
+            {
+                leftPreEnd++;
+            }
+            
+            root.left = BuildTree(preorder, inorder, sPre+1, leftPreEnd - 1, sIn, rootPositionInOrder - 1);
+            root.right = BuildTree(preorder, inorder, leftPreEnd, ePre, rootPositionInOrder + 1, eIn);
+
+            return root;
+        }
+
+        /*
+         // easy to understand solution
+          public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            if (preorder == null || preorder.Length == 0 || inorder == null || inorder.Length == 0)
+            {
+                return null;
+            }
+
+            if (preorder.Length != inorder.Length)
+            {
+                throw new Exception("Length of both arrays must be same");
+            }
+
+            var root = new TreeNode(preorder[0]);
+            var leftPreorder = new List<int>();
+            var rightPreorder = new List<int>();
+            var leftInorder = new List<int>();
+            var rightInorder = new List<int>();
+            var set = new HashSet<int>();
+            var rootFound = false;
+            foreach (var t in inorder)
+            {
+                if (t == preorder[0])
+                {
+                    rootFound = true;
+                    continue;//skip this item because it is root
+                }
+
+                if (rootFound)
+                {
+                    rightInorder.Add(t);
+                }
+                else
+                {
+                    leftInorder.Add(t);
+                    set.Add(t);
+                }
+            }
+
+            for (var i = 1; i < preorder.Length; i++)
+            {
+                if (set.Contains(preorder[i]))
+                {
+                    leftPreorder.Add(preorder[i]);
+                }
+                else
+                {
+                    rightPreorder.Add(preorder[i]);
+                }
+            }
+
+            root.left = BuildTree(leftPreorder.ToArray(), leftInorder.ToArray());
+            root.right = BuildTree(rightPreorder.ToArray(), rightInorder.ToArray());
+
+            return root;
+        }
+         
+         
+         */
+    }
+}
