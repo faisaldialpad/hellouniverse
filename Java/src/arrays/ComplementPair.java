54fd82ca400b7c55868f8815d1a3822f06dd4442
Faisal Rahman <md.faisal.rahman@gmail.com>
2017-10-05 21:11:07 -0700

    Complementpair updated

--- Java/src/arrays/ComplementPair.java
+++ Java/src/arrays/ComplementPair.java
@@ -1,29 +1,64 @@
 package arrays;
 
 import java.util.ArrayList;
-
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class ComplementPair {
-	private int complementNumber;
-	public ArrayList<HashSet<Integer>> result;
-	private int[] hashTable;
-	public ComplementPair(int complementNumber){
-		this.complementNumber=complementNumber;
-		result= new ArrayList<HashSet<Integer>>();
-		hashTable= new int[complementNumber+1];
-	}
-	public void add(int n){
-		if(n<0 || n>complementNumber) return;
-		else if(hashTable[complementNumber-n]>0){
-			hashTable[complementNumber-n]--;
-			HashSet<Integer> h=new HashSet<Integer>();
-			h.add(n);
-			h.add(complementNumber-n);
-			result.add(h);
+	public static ArrayList<HashSet<Integer>> GetAllComplementaryPairs(int[] nums, int target){
+		ArrayList<HashSet<Integer>> result = new ArrayList<HashSet<Integer>>();
+		if(nums == null || nums.length == 0) {
+			return result;
 		}
-		else{
-			hashTable[n]++;
+		
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for(int i=0; i<nums.length;i++) {
+			int complementKey = target - nums[i];
+			if(map.containsKey(complementKey) && map.get(complementKey) >= 1) {
+				map.put(complementKey, map.get(complementKey) - 1);
+				result.add(MakePair(complementKey, nums[i]));
+			}
+			else {
+				if(!map.containsKey(nums[i])) {
+					map.put(nums[i], 1);
+				}
+				else {
+					map.put(nums[i], map.get(nums[i]) + 1);
+				}
+			}
 		}
+		
+		return result;
 	}
+	
+	private static HashSet<Integer> MakePair(int num1, int num2){
+		HashSet<Integer> pair=new HashSet<Integer>();
+		pair.add(num1);
+		pair.add(num2);
+		return pair;
+	}
+	
+	// this method is optimal as per leetcode
+	public static int[] GetFirstComplementaryPairIndex(int[] nums, int target){
+		if(nums == null || nums.length == 0) {
+			return null;
+		}
+		
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for(int i=0; i<nums.length;i++) {
+			int complementKey = target - nums[i];
+			if(map.containsKey(complementKey)) {
+				return new int[] {map.get(complementKey), i};
+			}
+			else {
+				map.put(nums[i], i); // save the index so that we can return it
+			}
+		}
+		
+		return null;
+	}
+	
 }
+ 
