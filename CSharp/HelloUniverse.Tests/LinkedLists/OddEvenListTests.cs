4a799b0d358cc3a68b46e5f60df77e87e19a1e15
Faisal Rahman <md.faisal.rahman@gmail.com>
2017-10-14 22:43:15 -0700

    odd/evn linkedlist

---
+++ CSharp/HelloUniverse.Tests/LinkedLists/OddEvenListTests.cs
@@ -0,0 +1,52 @@
+﻿using HelloUniverse.LinkedLists;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace HelloUniverse.Tests.LinkedLists
+{
+    [TestFixture]
+    public class OddEvenListTests
+    {
+        [Test]
+        public void CornerCases()
+        {
+            Assert.IsNull(OddEvenList.GetOddEvenList(null));
+            Assert.AreEqual("5", OddEvenList.GetOddEvenList(new OddEvenList.ListNode(5)).ToString());
+            var two = new OddEvenList.ListNode(4)
+            {
+                next = new OddEvenList.ListNode(5)
+            };
+
+            Assert.AreEqual("4->5", OddEvenList.GetOddEvenList(two).ToString());
+        }
+
+        [Test]
+        public void NormalCases()
+        {
+            var three = new OddEvenList.ListNode(4)
+            {
+                next = new OddEvenList.ListNode(5)
+                {
+                    next = new OddEvenList.ListNode(6)
+                }
+            };
+
+            Assert.AreEqual("4->6->5", OddEvenList.GetOddEvenList(three).ToString());
+
+            var four = new OddEvenList.ListNode(4)
+            {
+                next = new OddEvenList.ListNode(5)
+                {
+                    next = new OddEvenList.ListNode(6)
+                    {
+                        next = new OddEvenList.ListNode(7)
+                    }
+                }
+            };
+
+            Assert.AreEqual("4->6->5->7", OddEvenList.GetOddEvenList(four).ToString());
+        }
+
+
+    }
+}
---
+++ CSharp/HelloUniverse/LinkedLists/OddEvenList.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace HelloUniverse.LinkedLists
+{
+    public static class OddEvenList
+    {
+        public static ListNode GetOddEvenList(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            var odd = head;
+            var evenHead = head.next;
+            var even = evenHead;
+
+            while (even?.next != null)
+            {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next; //odd.next is basically even.next.next in the last iteration. So, loop needs to check even.next!=null
+                even = even.next;
+            }
+
+            odd.next = evenHead;
+
+            return head;
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+
+            public ListNode(int x)
+            {
+                val = x;
+            }
+
+            public override string ToString()
+            {
+                var temp = this;
+                var list = new List<int>();
+                while (temp != null)
+                {
+                    list.Add(temp.val);
+                    temp = temp.next;
+                }
+
+                return string.Join("->", list.ToArray());
+            }
+        }
+    }
+}
