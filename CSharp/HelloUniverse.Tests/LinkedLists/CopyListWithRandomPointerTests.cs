c40f53273534f65529ecfcd2cffa9a3916d74cba
Faisal Rahman <md.faisal.rahman@gmail.com>
2017-10-15 00:13:01 -0700

    copy list with random pointer

---
+++ CSharp/HelloUniverse.Tests/LinkedLists/CopyListWithRandomPointerTests.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace HelloUniverse.Tests.LinkedLists
+{
+    [TestFixture]
+    public class CopyListWithRandomPointerTests
+    {
+        public delegate CopyListWithRandomPointer.RandomListNode CopyDelegate(CopyListWithRandomPointer.RandomListNode head);
+
+        [TestCaseSource(nameof(TestCases))]
+        public void TestCopy(CopyDelegate copy)
+        {
+            Assert.IsNull(copy(null));
+            Assert.IsTrue(AreSame(
+                new CopyListWithRandomPointer.RandomListNode(5),
+                copy(new CopyListWithRandomPointer.RandomListNode(5))));
+
+            var l1 = new CopyListWithRandomPointer.RandomListNode(5)
+            {
+                next = new CopyListWithRandomPointer.RandomListNode(6)
+                {
+                    next = new CopyListWithRandomPointer.RandomListNode(7)
+                    {
+                        next = new CopyListWithRandomPointer.RandomListNode(8)
+                    }
+                }
+            };
+
+            l1.random = l1.next.next;
+            l1.next.random = null;
+            l1.next.next.random = l1.next;
+            l1.next.next.random = l1;
+
+            Assert.IsTrue(AreSame(
+                l1,
+                copy(l1)));
+        }
+        private bool AreSame(
+            CopyListWithRandomPointer.RandomListNode l1,
+            CopyListWithRandomPointer.RandomListNode l2)
+        {
+            while (l1 != null && l2 != null)
+            {
+                if (l1.label != l2.label)
+                {
+                    return false;
+                }
+
+                if (l1.random != null && l2.random != null)
+                {
+                    if (l1.random.label != l2.random.label)
+                    {
+                        return false;
+                    }
+                }
+                else if (l1.random != null || l2.random != null)
+                {
+                    return false;
+                }
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            if (l1 != null || l2 != null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return
+                    new TestCaseData(new CopyDelegate(CopyListWithRandomPointer.CopyUsingMap)).SetName(
+                        nameof(CopyListWithRandomPointer.CopyUsingMap));
+                yield return
+                    new TestCaseData(new CopyDelegate(CopyListWithRandomPointer.CopyWithoutMap)).SetName(
+                        nameof(CopyListWithRandomPointer.CopyWithoutMap));
+            }
+        }
+    }
+}
---
+++ CSharp/HelloUniverse/LinkedLists/CopyListWithRandomPointer.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace HelloUniverse.Tests.LinkedLists
+{
+    public static class CopyListWithRandomPointer
+    {
+        public static RandomListNode CopyUsingMap(RandomListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            var map = new Dictionary<RandomListNode, RandomListNode>();
+            var temp = head;
+            while (temp != null)
+            {
+                map.Add(temp, new RandomListNode(temp.label));
+                temp = temp.next;
+            }
+
+            foreach (var item in map)
+            {
+                item.Value.next = item.Key.next != null ? map[item.Key.next] : null;
+                item.Value.random = item.Key.random != null ? map[item.Key.random] : null;
+            }
+
+            return map[head];
+        }
+
+        public static RandomListNode CopyWithoutMap(RandomListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            var temp = head;
+            while (temp != null)
+            {
+                var next = temp.next;
+                temp.next = new RandomListNode(temp.label)
+                {
+                    next = next
+                };
+                temp = next;
+            }
+            //A->B->C becomes A->A'->B->B'->C->C'
+
+            temp = head;
+            while (temp != null)
+            {
+                temp.next.random = temp.random?.next; //made a mistake here! forgot ?
+                temp = temp.next.next;
+            }
+            //A'->random must be A->random->next or (A->random)'
+
+            var copyHead = head.next;
+
+            temp = head; //odd numbered
+            var tempCopy = copyHead; //even numbered
+            while (tempCopy?.next != null)
+            {
+                temp.next = tempCopy.next;
+                temp = temp.next;
+                tempCopy.next = temp.next;
+                tempCopy = tempCopy.next;
+            }
+
+            temp.next = null; //forgot this
+            //both lists separated
+
+            return copyHead;
+        }
+
+        public class RandomListNode
+        {
+            public int label;
+            public RandomListNode next, random;
+
+            public RandomListNode(int x)
+            {
+                this.label = x;
+            }
+        };
+
+    }
+}
