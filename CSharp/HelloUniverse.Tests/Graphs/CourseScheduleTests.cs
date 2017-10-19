4a2afb1a2c16ff58e69a1ebfa533eedb6fcb3c28
Faisal Rahman <frahman@microsoft.com>
2017-10-19 10:39:48 -0700

    topological ordering added

--- CSharp/HelloUniverse.Tests/Graphs/CourseScheduleTests.cs
+++ CSharp/HelloUniverse.Tests/Graphs/CourseScheduleTests.cs
@@ -7,11 +7,18 @@ namespace HelloUniverse.Tests.Graphs
     public class CourseScheduleTests
     {
         [Test]
-        public void TestCases()
+        public void TestCanFinish()
         {
             Assert.IsTrue(CourseSchedule.CanFinish(3, new int[,] {{1, 0}, {2, 0}, {2, 1}}));
             Assert.IsFalse(CourseSchedule.CanFinish(3, new int[,] { { 1, 0 }, { 0, 1 } }));
         }
 
+        [Test]
+        public void TestFindOrder()
+        {
+            Assert.AreEqual(new int[] { 0, 1, 2, 3 }, CourseSchedule.FindOrder(4, new int[,] { { 1, 0 }, { 2, 0 }, { 3, 1 }, { 3, 2 } })); // can have multiple solution
+            Assert.AreEqual(new int[] {0, 1, 2}, CourseSchedule.FindOrder(3, new int[,] {{1, 0}, {2, 0}, {2, 1}}));
+            Assert.AreEqual(new int[] { }, CourseSchedule.FindOrder(3, new int[,] {{1, 0}, {0, 1}}));
+        }
     }
 }
--- CSharp/HelloUniverse/Graphs/CourseSchedule.cs
+++ CSharp/HelloUniverse/Graphs/CourseSchedule.cs
@@ -21,9 +21,10 @@ namespace HelloUniverse.Graphs
                 adjacencyList[prerequisites[i, 0]].Add(prerequisites[i, 1]);
             }
 
+            var topologicalOrder = new List<int>();
             for (var i = 0; i < numCourses; i++)
             {
-                if (states[i] == State.White && !Dfs(i, adjacencyList, states))
+                if (states[i] == State.White && !Dfs(i, adjacencyList, states, topologicalOrder))
                 {
                     return false;
                 }
@@ -32,7 +33,36 @@ namespace HelloUniverse.Graphs
             return true;
         }
 
-        private static bool Dfs(int node, HashSet<int>[] adjacencyList, State[] states)
+        public static int[] FindOrder(int numCourses, int[,] prerequisites) //topological order
+        {
+            var states = new State[numCourses];
+            var adjacencyList = new HashSet<int>[numCourses];
+            for (var i = 0; i < numCourses; i++)
+            {
+                adjacencyList[i] = new HashSet<int>();
+                states[i] = State.White;
+            }
+
+            // this is how you do it 
+            var rows = prerequisites.GetLength(0);
+            for (var i = 0; i < rows; i++)
+            {
+                adjacencyList[prerequisites[i, 0]].Add(prerequisites[i, 1]);
+            }
+
+            var topologicalOrder = new List<int>();
+            for (var i = 0; i < numCourses; i++)
+            {
+                if (states[i] == State.White && !Dfs(i, adjacencyList, states, topologicalOrder))
+                {
+                    return new int[0];
+                }
+            }
+
+            return topologicalOrder.ToArray();
+        }
+
+        private static bool Dfs(int node, HashSet<int>[] adjacencyList, State[] states, List<int> topologicalOrder/*not needed for CanFinish*/)
         {
             if (states[node] == State.Black)
             {
@@ -48,14 +78,14 @@ namespace HelloUniverse.Graphs
 
             foreach (var i in adjacencyList[node])
             {
-                if (!Dfs(i, adjacencyList, states))
+                if (!Dfs(i, adjacencyList, states, topologicalOrder))
                 {
                     return false;
                 }
             }
 
             states[node] = State.Black; //all its neibours are completed
-
+            topologicalOrder.Add(node);
             return true;
         }
 
