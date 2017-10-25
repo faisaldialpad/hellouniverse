d391e1e705298db73dee81bb8a0a89cb878e6c67
Faisal Rahman <md.faisal.rahman@gmail.com>
2017-10-24 18:05:17 -0700

    description added for a closed problem

--- CSharp/HelloUniverse/Graphs/AlienOrderTopologicalSort.cs
+++ CSharp/HelloUniverse/Graphs/AlienOrderTopologicalSort.cs
@@ -4,6 +4,13 @@ using System.Linq;
 
 namespace HelloUniverse.Graphs
 {
+    /*
+     * There is a new alien language which uses the latin alphabet. 
+     * However, the order among letters are unknown to you. 
+     * You receive a list of non-empty words from the dictionary, 
+     * where words are sorted lexicographically by the rules of this new language. 
+     * Derive the order of letters in this language.
+     */
     public static class AlienOrderTopologicalSort
     {
         public static string Sort(String[] words)
