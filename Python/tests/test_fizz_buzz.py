44c101d888f57130aa4e55a41ac6dbe330318397
Faisal Rahman <md.faisal.rahman@gmail.com>
2017-10-31 23:41:22 -0700

    fizzbuzz added

---
+++ Python/tests/test_fizz_buzz.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from dev.fizz_buzz import FizzBuzz
+
+
+class TestFizzBuzz(TestCase):
+    def test_get_fizz_buzz(self):
+        self.assertEqual(["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"], FizzBuzz.get_fizz_buzz(15))
