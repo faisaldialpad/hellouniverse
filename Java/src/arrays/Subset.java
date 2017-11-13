568975744be4e54b573ce1599704fb6ce1da9692
Faisal Rahman <md.faisal.rahman@gmail.com>
2017-11-12 19:46:38 -0800

    subset fixed

--- Java/src/arrays/Subset.java
+++ Java/src/arrays/Subset.java
@@ -1,17 +1,92 @@
 package arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 
 public class Subset {
-	public Subset() {}
-	public static HashSet<ArrayList<Integer>> subset(ArrayList<Integer> set){
+	//leet accepted
+	public static List<List<Integer>> getSubsetsIteratively(int[] nums) {
+		List<List<Integer>> ret = new ArrayList<>();
+		ret.add(new ArrayList<Integer>());
+		
+		if(nums == null || nums.length == 0) {
+			return ret;
+		}
+		// {1} -> {}, {1}
+		// {1, 2} -> {}, {1}, {2}, {1,2} (so add the new item to the previous result)
+		for(int i=0;i<nums.length;i++) {
+			int lastIterationLength = ret.size();
+			for(int j=0;j<lastIterationLength;j++) {
+				List<Integer> newSubset = new ArrayList<Integer>(ret.get(j));
+				newSubset.add(nums[i]);
+				ret.add(newSubset);
+			}
+		}
+		
+		return ret;
+	}
+	
+	//leet accepted
+	public static List<List<Integer>> getSubsetsBacktracking(int[] nums) {
+		if(nums == null) {
+			return null;
+		}
+		Arrays.sort(nums);
+		List<List<Integer>> result = new ArrayList<>();
+		getSubsetsBacktracking(nums, 0, new ArrayList<Integer>(), result);
+		
+		return result;
+	}
+	public static void getSubsetsBacktracking(
+			int[] nums, 
+			int start, 
+			ArrayList<Integer> currentResult,
+			List<List<Integer>> totalResult) 
+	{
+		totalResult.add(new ArrayList<Integer>(currentResult));
+		for(int i=start;i<nums.length;i++) {
+			currentResult.add(nums[i]);
+			getSubsetsBacktracking(nums, i + 1, currentResult, totalResult);
+			currentResult.remove(currentResult.size() -1);
+		}
+	}
+		
+	//leet accepted
+	public static List<List<Integer>> getSubsetsRecursively(int[] nums) {
+		if(nums == null) {
+			return null;
+		}
+		
+		return getSubsetsRecursively(nums, nums.length);
+	}
+	
+	//same concept as iterative but implemented recursively
+	private static List<List<Integer>> getSubsetsRecursively(int[] nums, int arrayLength) {
+		if(arrayLength == 0) {
+			List<List<Integer>> ret = new ArrayList<>();
+			ret.add(new ArrayList<Integer>());
+			return ret;
+		}
+		
+		List<List<Integer>> prev = getSubsetsRecursively(nums, arrayLength - 1);
+		int prevLength = prev.size();
+		for(int i=0;i<prevLength;i++) {
+			List<Integer> copy = new ArrayList<Integer>(prev.get(i));
+			copy.add(nums[arrayLength - 1]);
+			prev.add(copy);
+		}
+		
+		return prev;
+	}
+	public static HashSet<ArrayList<Integer>> getSubsetsRecursivelyOldWay(ArrayList<Integer> set){
 		if(set==null) return null;
 		HashSet<ArrayList<Integer>> ret=new HashSet<ArrayList<Integer>>();
 		ret.add( new ArrayList<Integer>(set));
 		for(int i=0;i<set.size();i++){
 			int temp=set.remove(i);
-			ret.addAll(subset(set));
+			ret.addAll(getSubsetsRecursivelyOldWay(set));
 			set.add(i, temp);
 		}
 		return ret;
--- Java/src/unittests/SubsetTest.java
+++ Java/src/unittests/SubsetTest.java
@@ -3,31 +3,148 @@ import static org.junit.Assert.*;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import arrays.Subset;
 
 
 public class SubsetTest {
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	@Test
+	public void testGetSubsetsIteratively() {
+		assertEquals(Arrays.asList(new ArrayList<Integer>()), Subset.getSubsetsIteratively(new int[] {}));
+		
+		assertEquals(
+				Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1)
+				), 
+				Subset.getSubsetsIteratively(new int[] {1}));
+		
+		assertEquals(
+				Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1),
+					Arrays.asList(2),
+					Arrays.asList(1, 2)
+				), 
+				Subset.getSubsetsIteratively(new int[] {1, 2}));
+		
+		assertEquals(
+				Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1),
+					Arrays.asList(2),
+					Arrays.asList(1, 2),
+					Arrays.asList(3),
+					Arrays.asList(1, 3),
+					Arrays.asList(2, 3),
+					Arrays.asList(1, 2, 3)
+				), 
+				Subset.getSubsetsIteratively(new int[] {1, 2, 3}));
+	}
+	
+	@Test
+	public void testGetSubsetsBacktracking() {
+		assertEquals(Arrays.asList(new ArrayList<Integer>()), Subset.getSubsetsBacktracking(new int[] {}));
+		
+		assertEquals(
+				Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1)
+				), 
+				Subset.getSubsetsBacktracking(new int[] {1}));
+		
+		assertEquals(
+				new HashSet<>(Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1),
+					Arrays.asList(2),
+					Arrays.asList(1, 2)
+				)), 
+				new HashSet<>(Subset.getSubsetsBacktracking(new int[] {1, 2})));
+		
+		assertEquals(
+				new HashSet<>(Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1),
+					Arrays.asList(2),
+					Arrays.asList(1, 2),
+					Arrays.asList(3),
+					Arrays.asList(1, 3),
+					Arrays.asList(2, 3),
+					Arrays.asList(1, 2, 3)
+				)), 
+				new HashSet<>(Subset.getSubsetsBacktracking(new int[] {1, 2, 3})));
+	}
+	@Test
+	public void testGetSubsetsRecursively() {
+		assertEquals(Arrays.asList(new ArrayList<Integer>()), Subset.getSubsetsRecursively(new int[] {}));
+		
+		assertEquals(
+				Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1)
+				), 
+				Subset.getSubsetsRecursively(new int[] {1}));
+		
+		assertEquals(
+				Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1),
+					Arrays.asList(2),
+					Arrays.asList(1, 2)
+				), 
+				Subset.getSubsetsRecursively(new int[] {1, 2}));
+		
+		assertEquals(
+				Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1),
+					Arrays.asList(2),
+					Arrays.asList(1, 2),
+					Arrays.asList(3),
+					Arrays.asList(1, 3),
+					Arrays.asList(2, 3),
+					Arrays.asList(1, 2, 3)
+				), 
+				Subset.getSubsetsRecursively(new int[] {1, 2, 3}));
 	}
 
 	@Test
-	public void testPermute() {
-		fail();
-		ArrayList<Integer> a= new ArrayList<Integer>();
-		a.add(1);
-		a.add(2);
-		a.add(3);
-		a.add(4);
+	public void testGetSubsetsRecursivelyOldWay() {
+		assertEquals(new HashSet<>(Arrays.asList(new ArrayList<Integer>())), Subset.getSubsetsRecursivelyOldWay(new ArrayList<Integer>()));
 		
-		HashSet<ArrayList<Integer>> ret=Subset.subset(a);
-		System.out.println(ret.toString());
+		assertEquals(
+				new HashSet<>(Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1)
+				)), 
+				Subset.getSubsetsRecursivelyOldWay(new ArrayList<Integer>(Arrays.asList(1))));
+		
+		assertEquals(
+				new HashSet<>(Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1),
+					Arrays.asList(2),
+					Arrays.asList(1, 2)
+				)), 
+				Subset.getSubsetsRecursivelyOldWay(new ArrayList<Integer>(Arrays.asList(1, 2))));
+		
+		assertEquals(
+				new HashSet<>(Arrays.asList(
+					new ArrayList<Integer>(),
+					Arrays.asList(1),
+					Arrays.asList(2),
+					Arrays.asList(1, 2),
+					Arrays.asList(3),
+					Arrays.asList(1, 3),
+					Arrays.asList(2, 3),
+					Arrays.asList(1, 2, 3)
+				)), 
+				Subset.getSubsetsRecursivelyOldWay(new ArrayList<Integer>(Arrays.asList(1, 2, 3))));
 	}
 
 }
